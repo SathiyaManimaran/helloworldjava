940ff49d7c64f1903da4f40c03de7278ac1e0473
Sathiya Arumugam <86502016+SathiyaManimaran@users.noreply.github.com>
2022-03-23 14:03:35 -0400

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,7 @@
 public class helloworld {
    public static void main(String[] args) {
     System.out.println("Hello World from GitHub using poll scm1");
+    System.out.println("Hello World from GitHub using poll scm2");  
+    System.out.println("Hello World from GitHub using poll scm3");
     }
 }
